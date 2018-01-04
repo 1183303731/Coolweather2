072afaf7ef447f7b213fb3bb20d837c301614fb3
Good Boy <1183303731@qq.com>
2018-01-05 01:40:11 +0800

    1111

--- app/src/main/java/com/example/goodboy/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/goodboy/coolweather/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.example.goodboy.coolweather;
 
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 import com.example.goodboy.coolweather.db.City;
 import com.example.goodboy.coolweather.db.County;
 import com.example.goodboy.coolweather.db.Province;
+import com.example.goodboy.coolweather.gson.Weather;
 import com.example.goodboy.coolweather.util.HttpUtil;
 import com.example.goodboy.coolweather.util.Utility;
 
@@ -83,6 +85,12 @@ public class ChooseAreaFragment extends android.support.v4.app.Fragment {
                 }else if(currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
---
+++ app/src/main/java/com/example/goodboy/coolweather/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.example.goodboy.coolweather.gson;
+
+/**
+ * Created by good boy on 2018/1/4.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
